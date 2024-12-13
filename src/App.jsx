e036078fad7e61117e4c02bc3a258066504fe972
Hansh6000 <hansharaj6000@gmail.com>
2024-12-13 19:06:37 +0530

    header created

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Homepage from "./pages/Homepage";
 import About from "./pages/About";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,39 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="bg-slate-200 shadow-md flex items-center justify-between max-w-6xl mx-auto p-3">
+      <h1 className="font-bol text-sm md:text-xl flex flex-wrap">
+        <Link to="/">
+          <span className="text-slate-400">Realfy</span>
+          <span className="text-slate-700">Estate</span>
+        </Link>
+      </h1>
+      <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="bg-transparent outline-none w-24 sm:w-64"
+        />
+        <FaSearch className="text-slate-300" />
+      </form>
+      <ul className="hidden md:flex gap-4 items-center mr-3">
+        <li className="text-slate-700 hover:underline transition-all duration-200">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="text-slate-700 hover:underline transition-all duration-200">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="text-slate-700 hover:underline transition-all duration-200">
+          <Link to="/signin">Signin</Link>
+        </li>
+      </ul>
+      <ul className="inline md:hidden">
+        <li className="text-slate-700 hover:underline transition-all duration-200">
+          <Link to="/signin">Signin</Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
